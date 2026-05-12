689b07214e29d8d027d890cc2d0c211a0e6dfe05
CaioBPFransozi <caio.fransozi@clinicaexperts.com.br>
2026-05-12 14:40:28 -0300

    adicionado .clang-format e função mais avançada do código

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	for (int i = 0; i <= 50; i++) {
-		printf("%d\n", i);
+	int size = 0;
+	printf("Bem-vindo ao gerador de tabuleiros de xadrez!\n");
+	printf("Informe o tamanho do lado do tabuleiro\n");
+	scanf("%d", &size);
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			printf("%d ", (j + i % 2) % 2);
+		}
+		printf("\n");
 	}
-	return 0;
+        return 0;
 }
