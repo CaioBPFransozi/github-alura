ee9ed81b2822dbad88f65b92840d73cd1d468170
CaioBPFransozi <caio.fransozi@clinicaexperts.com.br>
2026-05-12 17:13:17 -0300

    criamos um conflito

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 int main() {
         int size = 0;
         int forma = 0;
-        printf("Bem-vindo ao gerador de formatos!\n");
-        printf("Informe a forma desejada\n");
+        printf("Linha conflitante\n");
+        printf("Informe o formato desejado\n");
         printf("1 - Tabuleiro\n2 - Diamante\n");
         scanf("%d", &forma);
         if (forma < 1 || forma > 2) {
